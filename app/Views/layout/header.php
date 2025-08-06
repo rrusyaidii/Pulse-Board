ece14f101dd185bd2e9b76e0b3311fcfba3303e9
Rusyaidi <116720602+rrusyaidii@users.noreply.github.com>
2025-08-06 11:21:58 +0800

    remove notification,darkmode and search on header

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -27,45 +27,6 @@
 
     <div class="nav-right col-xxl-7 col-xl-6 col-md-7 col-8 pull-right right-header p-0 ms-auto">
       <ul class="nav-menus">
-
-        <li> <span class="header-search">
-            <svg>
-              <use href="<?= base_url() ?>/assets/svg/icon-sprite.svg#search"></use>
-            </svg></span></li>
-        
-        <li>
-          <div class="mode">
-            <svg>
-              <use href="<?= base_url() ?>/assets/svg/icon-sprite.svg#moon"></use>
-            </svg>
-          </div>
-        </li>
-     
-        <li class="onhover-dropdown">
-          <div class="notification-box">
-            <svg>
-              <use href="<?= base_url() ?>/assets/svg/icon-sprite.svg#notification"></use>
-            </svg><span class="badge rounded-pill badge-secondary">4 </span>
-          </div>
-          <div class="onhover-show-div notification-dropdown">
-            <h6 class="f-18 mb-0 dropdown-title">Notitications </h6>
-            <ul>
-              <li class="b-l-primary border-4">
-                <p>Delivery processing <span class="font-danger">10 min.</span></p>
-              </li>
-              <li class="b-l-success border-4">
-                <p>Order Complete<span class="font-success">1 hr</span></p>
-              </li>
-              <li class="b-l-secondary border-4">
-                <p>Tickets Generated<span class="font-secondary">3 hr</span></p>
-              </li>
-              <li class="b-l-warning border-4">
-                <p>Delivery Complete<span class="font-warning">6 hr</span></p>
-              </li>
-              <li><a class="f-w-700" href="#">Check all</a></li>
-            </ul>
-          </div>
-        </li>
         <li class="profile-nav onhover-dropdown pe-0 py-0">
           <div class="media profile-media"><img class="b-r-10" src="<?= base_url() ?>/assets/images/logo/zoro.jpg" alt="" style="height: 32px; width: 32px; object-fit: cover;">
             <div class="media-body"><span><?= session()->get('name') ?></span>
