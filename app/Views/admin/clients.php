929a915a9ffe217947caa562dddee7d38b5a95d7
AfiqahHalim <fiqa0105@gmail.com>
2025-08-06 09:59:04 +0800

    bugs css

--- app/Views/admin/clients.php
+++ app/Views/admin/clients.php
@@ -58,11 +58,10 @@
 </div>
 
 <!-- DataTables -->
-<!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script> -->
-<?= $this->endSection() ?>
-<?= $this->section('script') ?>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+
 <script>
   $(document).ready(function () {
     $('#userTable').DataTable({
